36bf59f010c0649fea47fce712c0818d3708b092
boyapati nagendra <nagen4boyapati@gmail.com>
2024-11-09 15:47:44 +0000

    modified for c string

--- string/string.cpp
+++ string/string.cpp
@@ -2,7 +2,19 @@
 #include <string>
 
 int main() {
-	std::string greetings = "Hello";
-	std::cout << greetings << " there" << std::endl;
+	std::string greeting = "Hello";
+	std::string complete_greeting = greeting + " there";
+	complete_greeting += "!";
+	std::cout << complete_greeting << std::endl;
+	std::cout << complete_greeting.length() << std::endl;
+
+	// method == member function == function attached to 
+	// objects
+	
+	char name[] = "Caleb"; // c string == array of characters "Caleb\0"
+	// name = "B";
+	
+
+	complete_greeting = "Go away";
 	return 0;
 }
