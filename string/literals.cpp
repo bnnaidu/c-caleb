5ba542db03fbcf9930ca0b66e8a735f4e3b009e1
bnnaidu <nagen4boyapati@gmail.com>
2024-11-10 10:56:16 +0530

    string literals is added

---
+++ string/literals.cpp
@@ -0,0 +1,14 @@
+#include "header.h"
+
+int main() {
+    int dec_num = 30; // thirty
+    cout << dec_num << endl;
+
+    int hex_num = 0x30; // forty eight
+    cout << hex_num << endl;
+
+    int oct_num = 030; // twenty four
+    cout << oct_num << endl;
+
+    return 0;
+}
--- string/methods.cpp
+++ string/methods.cpp
@@ -1,11 +1,13 @@
 #include "header.h"
 
 int main() {
-    string greeting="hello";
+    string greeting="what is hell";
     // greeting.insert(3," ");
-    greeting.erase(greeting.length()-1);
-    cout << greeting << endl;
-
+    // greeting.erase(greeting.length()-1);
+    // greeting.pop_back();
+    // greeting.replace(greeting.find("hell"), 4, "****");
+    // if(greeting.find_first_of("!") == -1) cout << "NOT FOUND" << endl;
+    if(greeting == "what is hell") cout << "Equals" << endl;
     return 0;
 }
 
